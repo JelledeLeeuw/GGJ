36cdc4bccebe0e16b20f0cc6a4a2a531f90ec939
Nqrds <rrvanleeuwen@kpnmail.nl>
2025-01-31 11:46:49 +0100

    Inputactionmap

---
+++ Assets/Developers/Input/Inputhandler.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputHandler : MonoBehaviour
+{
+    [Header("InputSystem")]
+    [SerializeField] private Controls playerControls;
+
+    private InputAction moveAction;
+    //private InputAction jumpAction;
+    private InputAction sprintAction;
+    private InputAction attackAction;
+    public Vector2 moveInput { get; private set; }
+
+    public float sprintValue { get; private set; }
+
+    public bool jumpTriggered { get; private set; }
+
+    public bool attackTriggered { get; private set; }
+
+    public static InputHandler Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        moveAction = playerControls.Player.Move;
+        RegisterInputActions();
+    }
+
+    private void RegisterInputActions()
+    {
+        moveAction.performed += context => moveInput = context.ReadValue<Vector2>();
+        moveAction.canceled += context => moveInput = Vector2.zero;
+    }
+
+    private void OnEnable()
+    {
+        moveAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        moveAction.Disable();
+    }
+}
