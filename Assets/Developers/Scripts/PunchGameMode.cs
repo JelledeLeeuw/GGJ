e898b6efdb76d6213ba85e3b51debd073ffd33da
Jesper Heese <77155517+RgbCat0@users.noreply.github.com>
2025-02-01 14:39:16 +0100

    Update PunchGameMode.cs

--- Assets/Developers/Scripts/PunchGameMode.cs
+++ Assets/Developers/Scripts/PunchGameMode.cs
@@ -1,15 +1,22 @@
-using System;
 using UnityEngine;
 
 public class PunchGameMode : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision other)
+    private void OnCollisionEnter(Collision collision)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player"))
         {
-            other
-                .gameObject.GetComponent<Rigidbody>()
-                .AddForce(other.transform.forward * -100, ForceMode.Impulse);
+            Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                // Get the direction **away from the collision point**
+                Vector3 knockbackDirection = (
+                    collision.transform.position - transform.position
+                ).normalized;
+
+                // Apply force in that direction
+                rb.AddForce(knockbackDirection * 10f, ForceMode.Impulse);
+            }
         }
     }
 }
